95f297aaeb8adda16b5ea0a9147d1cd07d8c9b60
nbakshi15 <nirali.bakshi@yahoo.com>
2016-09-08 09:49:39 -0400

    testinggg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.println("what does this do");
+        //withing parenthesis there is a set of parameters
+        //yes this is a single line comment
+
+        /* This
+        is
+        a
+        block
+        comment
+         */
     }
 }
